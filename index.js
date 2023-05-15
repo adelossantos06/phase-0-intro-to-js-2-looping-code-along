b8fdce1ce6309e6965e9af74b0cff37c23a0bf33
Alyssa De Los Santos <adelossantos6@gmail.com>
2023-05-15 17:20:37 -0500

    completes looping code along

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+names = ['Guadalupe', 'Ollie', 'Aki'];
+
+function writeCards(names){
+    let cards = [];
+    for (let i = 0; i < names.length ; i++ ){
+        cards.push (`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+    }
+    return (cards);
+}
+
+function countDown(){
+    for (let i = 10; i >= 0; i--){
+        console.log(i);
+    }
+}
